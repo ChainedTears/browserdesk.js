0c2b166d7460f758135511ba78f08c3ed46462e4
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-10-02 21:23:53 -0700

    Update browserdesk.js

--- browserdesk.js
+++ browserdesk.js
@@ -239,7 +239,7 @@ const welcome = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to DuckOS v6 Beta!</title>
+    <title>Browserdesk</title>
     <style>
         .container {
             max-width: 800px;
@@ -261,128 +261,19 @@ const welcome = `
     </style>
 </head>
 <body>
-    <div class="container">
-        <h1 style="color: red;">This window will only show up once.</h1>
-        <h2>Welcome to DuckOS v6!</h2>
-        <p>Embark on a New Digital Journey with Unrivaled Features<br><small>(Continuously Evolving)</small><br>Just Launched – Embrace the Excitement!<br><small>Ongoing Development to Enhance Your Experience</small></p>
-        <p style="color: red;">Remember to enter Fullscreen so DuckOS formats correctly!</p>
-        <p>Get Ready for a Polished Landing Page Coming Your Way</p>
-        <hr>
-        <p>Discover DuckOS v6: A Glimpse into the Future</p>
-        <p>Welcome to DuckOS v6, where innovation meets accessibility. As you step into the world of DuckOS, you're entering an unparalleled web-based operating system, meticulously designed to redefine your digital experience. Although this version is still in its beta phase, the journey ahead promises a new era of computing freedom.</p>
-        <p>Unleash Your Creativity with Customizable Settings</p>
-        <p>DuckOS understands that each user is unique. That's why we've made customization a cornerstone of this OS. Tailor your environment to your preferences – adjust themes, layouts, and even workflows. Personalize your workspace like never before, reflecting your individuality.</p>
-        <p>Game On: Entertainment at Your Fingertips</p>
-        <p>Get ready to game like never before. DuckOS v6 comes equipped with a vast array of captivating games suitable for all ages. Whether you're a casual gamer or a hardcore enthusiast, our game library has something to offer. With intuitive controls and seamless integration, your gaming adventures are just a click away.</p>
-        <p>Unrestricted Access with Built-In Proxies</p>
-        <p>Say goodbye to online barriers. DuckOS v6 comes with the best flagship proxies such as Cobalt, Rammerhead and Immortal.</p>
-    </div>
+    <h1>Browserdesk has been sucessfully installed!</h1>
+    <p>This message will only show up once for new users. If you would like to remove this window, just delete the code.</p>
 </body>
 </html>
 
 `
 
 if (!localStorage.getItem('visitedBefore')) {
-  createWindow('Welcome to DuckOS!', welcome, '50%', '50%');
+  createWindow('Browserdesk.js Successfully Installed!', welcome, '50%', '50%');
   localStorage.setItem('visitedBefore', 'true');
 }
 
-// App's code
 
-const proxy = `
-<iframe src="/system/apps/proxies/index.html" frameborder="0" style="width: 100%; height: 100%; border: none;"></iframe>
-`
-
-const calculatorApp = `
-<iframe id="ifr" src="/system/apps/calculator/index.html" style="width:100%; height:100%;" frameborder="none"></iframe>
-`;
-
-const games = `
-<iframe id="ifr" src="/system/apps/games/index.html" style="width:100%; height:100%;" frameborder="none"></iframe>
-`
-const terminal = `
-<iframe id="ifr" src="/system/apps/terminal/index.html" style="width:100%; height:100%;" frameborder="none"></iframe>
-`
-
-// List of apps 
-const appsContainer = document.getElementById('appsContainer');
-const data = [{
-  id: 1,
-  imageSrc: '/media/icons/proxy.png',
-  text: 'Proxies',
-  clickFunction: function() {
-    createWindow('Proxies', proxy, '60%', '70%');
-  }
-},
-{
-  id: 2,
-  imageSrc: '/media/icons/games.png',
-  text: 'Games',
-  clickFunction: function() {
-    createWindow('DuckOS Games', games, '40%', '60%');
-  }
-},
-{
-  id: 3,
-  imageSrc: '/media/icons/calculator.png',
-  text: 'Calculator',
-  clickFunction: function() {
-    createWindow('Calculator App', calculatorApp, '15%', '50%');
-  }
-},
-{
-  id: 4,
-  imageSrc: '/media/icons/terminal.png',
-  text: 'Terminal',
-  clickFunction: function() {
-    createWindow('Terminal Emulator', terminal, '40%', '45%');
-  }
-},
-];
-
-function createApp(appData) {
-  const miniDiv = document.createElement('div');
-  miniDiv.classList.add('appsbox');
-
-  miniDiv.setAttribute('data-app-id', appData.id);
-
-  const image = document.createElement('img');
-  image.src = appData.imageSrc;
-  miniDiv.appendChild(image);
-
-  miniDiv.addEventListener('click', appData.clickFunction);
-
-  appsContainer.appendChild(miniDiv);
-}
-
-function removeAppById(appId) {
-  const appDivToRemove = appsContainer.querySelector(`[data-app-id="${appId}"]`);
-  if (appDivToRemove) {
-    appsContainer.removeChild(appDivToRemove);
-  }
-}
-
-// Add apps using the createApp function
-data.forEach(item => {
-  createApp(item);
-});
-
-// Example usage for devs: Remove the app with ID 2
-// removeAppById(2);
-// Example usage for devs: Remove latest added app
-// removeApp(1);
-
-/*
-    const newAppData = {
-        id: 39384,
-        imageSrc: '/image/path',
-        text: 'Name of App',
-        clickFunction: function() {
-            createWindow('App window name', 'innerHTML Content');
-        }
-    };
-   
-    createApp(newAppData);
 */
 
 // IT IS RECOMMENDED TO USE STYLES INSIDE THE HTML TAGS, LIKE I DID ABOVE.
